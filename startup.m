15f7db412d7e1da5a63e6f6be9958caae3d0cca6
Kem White <kemwhite@outlook.com>
2020-09-13 09:39:18 -0400

    Update startup.m for home laptop use.

--- startup.m
+++ startup.m
@@ -5,8 +5,8 @@ ft2m=0.3048;
 m2ft = 1.0/ft2m;
 format shortg;
 format compact;
-dev_loc_root = 'C:\Users\whitedk1\Documents\GitLab\atep-g\';
-cd 'C:\Users\whitedk1\Documents\MATLAB';
+% dev_loc_root = 'C:\Users\whitedk1\Documents\GitLab\atep-g\';
+cd 'C:\Users\kemwh\OneDrive\Documents\MATLAB';
 
 
 
