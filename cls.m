7c2de135ac441e82c628d7ea9169c6f7d10d0d82
Kem White <kemwhite@outlook.com>
2021-06-11 19:20:21 -0400

    Slight Tweakd

--- cls.m
+++ cls.m
@@ -1,5 +1,6 @@
 %
 %    Alias for clc
+%    I've used this alias forever
 %
 %    Simple clear screen using old DOS command syntax*
 %    This alias was the name of an alias in an old DOS shell program I used.
